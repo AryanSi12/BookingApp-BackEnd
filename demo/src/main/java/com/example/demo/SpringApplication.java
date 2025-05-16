05a6616d5fddeb9a01543afc68232d62c5110f67
AryanSi12 <145208787+AryanSi12@users.noreply.github.com>
2025-05-17 02:00:12 +0530

    redis

--- demo/src/main/java/com/example/demo/SpringApplication.java
+++ demo/src/main/java/com/example/demo/SpringApplication.java
@@ -14,18 +14,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpringApplication {
 
 	public static void main(String[] args) {
-		String redisUrl = System.getenv("REDIS_URL");
-		RedisURI redisURI = RedisURI.create(redisUrl);
-		RedisClient client = RedisClient.create(redisURI);
-
-		try (var connection = client.connect()) {
-			RedisCommands<String, String> commands = connection.sync();
-			commands.set("foo", "bar");
-			String value = commands.get("foo");
-			System.out.println("foo = " + value);
-		}
-
-		client.shutdown();
 		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
 	}
 
