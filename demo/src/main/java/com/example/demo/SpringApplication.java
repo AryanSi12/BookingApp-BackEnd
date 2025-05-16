1b9d12b2639f03143293cdce0155855451dcfb3d
AryanSi12 <145208787+AryanSi12@users.noreply.github.com>
2025-05-17 01:44:30 +0530

    redis

--- demo/src/main/java/com/example/demo/SpringApplication.java
+++ demo/src/main/java/com/example/demo/SpringApplication.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.sync.RedisCommands;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +14,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpringApplication {
 
 	public static void main(String[] args) {
+		String redisUrl = System.getenv("REDIS_URL");
+		RedisURI redisURI = RedisURI.create(redisUrl);
+		RedisClient client = RedisClient.create(redisURI);
 
+		try (var connection = client.connect()) {
+			RedisCommands<String, String> commands = connection.sync();
+			commands.set("foo", "bar");
+			String value = commands.get("foo");
+			System.out.println("foo = " + value);
+		}
+
+		client.shutdown();
 		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
 	}
 
